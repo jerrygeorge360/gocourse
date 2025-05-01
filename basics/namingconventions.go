121391044234169e2dd13e1697ff0275a4c08fcc
jerry george <jerrygithub360@gmail.com>
2025-05-01 10:34:52 +0100

    initial commit

---
+++ basics/namingconventions.go
@@ -0,0 +1,12 @@
+package basics
+
+type Employee struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func main() {
+	const MAXRETRIES = 5
+
+}
---
+++ basics/variable.go
@@ -0,0 +1,16 @@
+package basics
+import "fmt"
+
+middleName := "Cane"
+func main(){
+	var age int 
+	var name string = "John"
+	var name1 = "Jane"
+
+	count := 10
+	lastName := "Smith"
+
+	fmt.Println(name)
+
+
+}
