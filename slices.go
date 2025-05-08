e2dcdfbcc44a7d372ff835b72fbfb610def5cd9b
Henry <hoyx0101@gmail.com>
2025-05-08 08:18:57 +0100

    Update slices.go
    Learning go with Jerry

--- slices.go
+++ slices.go
@@ -5,5 +5,11 @@ func main() {
 	var numbers1 = []int{1,2,3}
 	numbers2 := []int{1}
 	slice := make([]int,5)
-	
+
+
+	fmt.Println("Care to know the capacity", cap(slice))
+
+	dummy := &slice
+
+	fmt.Println(*dummy[2:5]) // can you guess what's contained in the array now?
 }
